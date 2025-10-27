a019bb7b8d487682bd2f8bc1d91804cafaa84400
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-27 16:32:01 -0600

    Sección 2: Componentes. - Clase 7. Children.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,11 @@
 import Card, { CardBody } from "./Components/Card";
 
 function App() {
-  return <CardBody title={"Hola Mundo"} text={"El texto del componente"} />;
+  return (
+    <Card>
+      <CardBody title="Hola Mundo" text="El texto del componente" />
+    </Card>
+  );
 }
 
 export default App;
