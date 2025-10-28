0665836aec16a779edb5bb62da560d4a55e4236a
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-27 18:43:09 -0600

    Sección 2: Componentes. - Clase 11. Funciones como props.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,10 +3,20 @@ import List from "./Components/List";
 
 function App() {
   const list = ["Goku", "Tanjiro", "Eren"];
+
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
+
+  const handleSelect2 = (elemento: string) => {
+    console.log("mostrando", elemento);
+  };
+
   return (
     <Card>
       <CardBody title="Hola Mundo" text="El texto del componente" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
     </Card>
   );
 }
