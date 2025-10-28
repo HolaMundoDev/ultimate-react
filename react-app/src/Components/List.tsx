9487a06cccdba4a26ad5cebae3cbe52583919c4f
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-27 18:19:52 -0600

    Sección 2: Componentes. - Clase 9. Gestión de eventos.

--- react-app/src/Components/List.tsx
+++ react-app/src/Components/List.tsx
@@ -1,12 +1,22 @@
+import { MouseEvent } from "react";
+
 type Props = {
   data: string[];
 };
 
 const List = ({ data }: Props) => {
+  const handleClick = (e: string) => {
+    console.log(e);
+  };
+
   return (
     <ul className="list-group">
       {data.map((elemento) => (
-        <li className="list-group-item" key={elemento}>
+        <li
+          onClick={() => handleClick(elemento)}
+          className="list-group-item"
+          key={elemento}
+        >
           {elemento}
         </li>
       ))}
