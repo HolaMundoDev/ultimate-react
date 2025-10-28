8765cac640fd5076d15bc0eff2c4a966e99a7a06
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-27 18:53:36 -0600

    Sección 2: Componentes. - Clase 13. Truthy y falsy en React.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,21 +2,22 @@ import Card, { CardBody } from "./Components/Card";
 import List from "./Components/List";
 
 function App() {
-  const list = ["Goku", "Tanjiro", "Eren"];
+  const list: string[] = [];
 
   const handleSelect = (elemento: string) => {
     console.log("imprimiendo", elemento);
   };
 
-  const handleSelect2 = (elemento: string) => {
-    console.log("mostrando", elemento);
-  };
-
   return (
     <Card>
+      {/* {"" && "string vacio"}
+      {undefined && "indefinido"}
+      {null && "nulo"}
+      {false && "falso"}
+      {0 && "0"} */}
+      {list.length !== 0 && "mi lista"}
       <CardBody title="Hola Mundo" text="El texto del componente" />
       <List data={list} onSelect={handleSelect} />
-      <List data={list} onSelect={handleSelect2} />
     </Card>
   );
 }
