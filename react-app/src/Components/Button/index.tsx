77973c1474a88e4110e16adc6b5351685a3feb97
Gabriel Hernandez <onedrakodev@gmail.com>
2025-12-12 19:32:16 -0600

    Sección 3: Estilos. - Clase 06. CSS en JS.

--- react-app/src/Components/Button/index.tsx
+++ react-app/src/Components/Button/index.tsx
@@ -1,27 +1,30 @@
 import { ReactNode } from "react";
-import styles from "./Button.module.css";
+import styled from "styled-components";
+
+type BtnProps = {
+  isLoading: boolean;
+}
+
+const Btn = styled.button<BtnProps>`
+  background-color: ${props => props.isLoading ? "gray" : "red"};
+  padding: 25px 30px;
+`;
 
 type Props = {
   children: ReactNode;
-  isLoading?: boolean;
+  isLoading: boolean;
   onClick: () => void;
 };
 
-console.log(styles.button);
-
 function Button({ children, isLoading, onClick }: Props) {
-  const className = [`btn btn-${isLoading ? "secondary" : "primary"}`, styles.button].join(" ");
-
   return (
-    <button
+    <Btn
       onClick={onClick}
       disabled={isLoading}
-      type="button"
-      // className={[styles.button, styles.padded].join(" ")}
-      className={className}
+      isLoading={isLoading}
     >
       {isLoading ? "Cargando..." : children}
-    </button>
+    </Btn>
   );
 }
 
